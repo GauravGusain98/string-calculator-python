bb9e9695621125d979a6c42a097a2e99c3b55908
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2025-04-12 00:13:09 +0530

    Test Case and Functionality to handle single number

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,6 +2,13 @@ def add(numbers):
     if not numbers:
         return 0
     
+    delimiter = ','
+    number_part = numbers
+    string_number_list = number_part.split(delimiter)
+    number_list = [int(num) for num in string_number_list]
+
+    return sum(number_list)
+    
 if __name__ == "__main__":
     input_string = input('Enter a string:')
     result = add(input_string)
