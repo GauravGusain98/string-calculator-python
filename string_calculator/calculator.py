3b7535b21c0451b6259a13adcdd76b917df68f0a
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2025-04-12 00:22:51 +0530

    Add test and functionality to handle custom delimiter

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -4,6 +4,11 @@ def add(numbers):
     
     delimiter = ','
     number_part = numbers
+    ## Handle custom delimiter
+    if numbers.startswith("//"):
+        delimiter_line, number_part = numbers.split("\n", 1)
+        delimiter = delimiter_line[2:]
+
     number_part = number_part.replace('\n', delimiter)
     string_number_list = number_part.split(delimiter)
     number_list = [int(num) for num in string_number_list]
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -17,6 +17,9 @@ class TestStringCalculator(unittest.TestCase):
 
     def test_newline_between_numbers(self):
         self.assertEqual(add("1\n2,3"), 6)
+
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
         
 if __name__ == "__main__":
     unittest.main()
