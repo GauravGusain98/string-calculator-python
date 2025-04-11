231c1565e4363638c54c2f5d406385d2d702c168
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2025-04-11 23:40:50 +0530

    Added Directory Structure, Readme, and first test case and functionality to handle empty string as input to calculator

---
+++ string_calculator/calculator.py
@@ -0,0 +1,8 @@
+def add(numbers):
+    if not numbers:
+        return 0
+    
+if __name__ == "__main__":
+    input_string = input('Enter a string:')
+    result = add(input_string)
+    print(f"Output is: {result}")
