24b6f7f38153ee43672ee8f662c3305a26ed3e26
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2025-04-12 00:17:58 +0530

    test with whitespaces

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,6 +12,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_two_numbers(self):
         self.assertEqual(add("1,2"), 3)
  
+    def test_multiple_numbers_with_whitespaces(self):
+        self.assertEqual(add(" 1,2, 6, 7 "), 16)
         
 if __name__ == "__main__":
     unittest.main()
